9d8559c67825b89bafceea0ccda45e669bf7ce5f
kipgeoffry <kipgeoffry@gmail.com>
2023-11-08 14:12:05 +0300

    added spark and handlebars.Tested routing to be ok

--- src/main/java/net/kigen/App.java
+++ src/main/java/net/kigen/App.java
@@ -1,9 +1,25 @@
 package net.kigen;
 
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
+        port(2100);
 
-
+        //get: show all heroes
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new HandlebarsTemplateEngine().render(
+                    new ModelAndView(model, "index.hbs")
+            );
+        });
     }
 }
