3b0f6306eb558181da784d32560479f5ff8e41ce
kipgeoffry <kipgeoffry@gmail.com>
2023-11-08 16:59:13 +0300

    added sl4J logging

--- src/main/java/net/kigen/App.java
+++ src/main/java/net/kigen/App.java
@@ -1,6 +1,8 @@
 package net.kigen;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
@@ -10,16 +12,19 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         staticFileLocation("/public");
-        port(2100);
+        port(2200);
 
-        //get: show all heroes
+//        //get: show all heroes
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             return new HandlebarsTemplateEngine().render(
                     new ModelAndView(model, "index.hbs")
             );
         });
+
+        logger.info("**** App is running and listening on port {} *** ",port());
     }
 }
